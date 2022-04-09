0dd9f6fe1052faa7fd5564c1724d68da96ea9de7
kuro_ye <erhanaket77@gmail.com>
2022-04-09 22:15:56 +0600

    add php body block

--- base.php
+++ base.php
@@ -10,7 +10,7 @@ require_once 'ti.php'
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="index.css" type="text/css">
+    <link rel="stylesheet" href="static/index.css" type="text/css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
 
@@ -75,6 +75,11 @@ require_once 'ti.php'
 
     </div>
 </nav>
+
+<div>
+    <?php startblock('body')?>
+    <?php endblock()?>
+</div>
 
 
 
