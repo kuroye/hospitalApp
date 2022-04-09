ddf20f27b78264ff9b15634d0676a29f90de45a7
kuro_ye <erhanaket77@gmail.com>
2022-04-09 23:12:28 +0600

    feat: function for connect to db

--- connect_database.php
+++ connect_database.php
@@ -1,1 +1,20 @@
 <?php
+
+
+function connect_database() {
+
+    $dsn = 'mysql:host=localhost;dbname=hospital';
+    $username = 'root';
+    $password = '';
+
+    try {
+        $pdo= new PDO($dsn,$username,$password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }
+    catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
+}
+
+?>
