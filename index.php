c418a9fa588d99cf059a3f177979a26da69e6963
kuro_ye <erhanaket77@gmail.com>
2022-04-09 22:23:38 +0600

    add block body

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 include 'base.php'
 ?>
 
-<?php startblock('title')?>
+<?php startblock('title');?>
 主页
-<?php endblock()?>
+<?php endblock();?>
+
+<?php startblock('body');?>
+<h1 style="text-align: center">Hello</h1>
+<?php endblock();?>
 
 
 
